904cd3c9140374b6fb375e24a94e8f750bbf7d07
GhostLixo <martinsclaudiojunior@gmail.com>
2026-03-15 22:02:01 -0300

    fgdfg

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(teste);
+void main (){
+    final teste = MaterialApp
+    (home: Scaffold(
+        backgroundColor: Color.fromARGB(246, 225, 140, 114),
+        body: Column
+            (children: [ Align(
+                alignment: Alignment.center, child:  SizedBox
+                (height: 200, width: 100,  
+                child: TextField
+                    (   textAlign: TextAlign.center,
+                        decoration: InputDecoration(hintText: "kjsbdbfj"))
+                ))]) )
+    );
+    runApp(teste);
 }
-
-const teste = MaterialApp(home: Center(child: Text("texto")));
